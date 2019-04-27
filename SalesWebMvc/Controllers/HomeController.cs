83a050b0c60f98d313c32c40a4b5c3ede17f80e2
fabioportodev <fabioportodev@gmail.com>
2019-04-27 08:43:43 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Fábio Porto é o Engenheiro responsável por esse projeto.";
+            ViewData["email"] = "fabioportodev@gmail.com";
 
             return View();
         }
